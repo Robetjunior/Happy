ae5b4c1de50bc7a8166848cacff19287dbc77541
Jose Roberto <juniorferreirarob@gmail.com>
2020-10-14 00:53:23 -0300

    first styles OrphanpagesMap

--- src/pages/OrphanpagesMap.tsx
+++ src/pages/OrphanpagesMap.tsx
@@ -1,8 +1,32 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import { FiPlus } from 'react-icons/fi'
+
+import mapMarkerImg from '../images/Local.svg';
 
 function OrpahnpagesMap(){
     return (
-        <h1>Hello World</h1>
+     <div className="page-map">
+         <aside>
+             <header>
+                 <img src={mapMarkerImg} alt="Happy"/>
+
+                 <h2>Escolha um orfanato do mapa</h2>
+                 <p>Muitas crianças estão esperando a sua visita :)</p>
+             </header>
+
+             <footer>
+                 <strong>Lapa</strong>
+                 <span>Sao Paulo</span>
+             </footer>
+         </aside>
+
+        <div></div>
+
+        <Link to="" className="create-orphanpage">
+            <FiPlus size={32} color="#fff" />
+        </Link>
+     </div>
     )
 }
 
