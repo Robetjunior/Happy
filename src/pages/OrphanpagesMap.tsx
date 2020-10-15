a15214250a885bde6ad6dbcb1c023a00b6a459af
Jose Roberto <juniorferreirarob@gmail.com>
2020-10-15 13:23:32 -0300

    update OrphanagesMap.tsx

--- src/pages/OrphanpagesMap.tsx
+++ src/pages/OrphanpagesMap.tsx
@@ -1,14 +1,18 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import { FiPlus } from 'react-icons/fi';
-import { Map, TileLayer } from 'react-leaflet';
+import { Map, TileLayer, Marker } from 'react-leaflet';
+import Leaflet from 'leaflet';
 
 import 'leaflet/dist/leaflet.css'
 
 import mapMarkerImg from '../images/Local.svg';
 
-
 import '../styles/pages/orphanages-map.css'
+
+const mapIcon = Leaflet.icon({
+    iconUrl: mapMarkerImg
+})
 
 function OrpahnpagesMap(){
     return (
@@ -32,10 +36,15 @@ function OrpahnpagesMap(){
             zoom={15}
             style={{ width: '100%', height: '100%'}}
         >
-            <TileLayer url="https://a.tile.openstreetmap.org/{z}/{x}/{y}.png" />
-            {/* <TileLayer 
-                url={`htpps://api.mapbox.com/styles/v1/mapbox/light-v10/tiles/256/{z}/{x}/{y}@2x?access_token=${process.env.REACT_APP_MAPBOX_TOKEN}`} 
-                /> */}
+            {/* <TileLayer url="https://a.tile.openstreetmap.org/{z}/{x}/{y}.png" /> */}
+            <TileLayer 
+                url={`https://api.mapbox.com/styles/v1/mapbox/light-v10/tiles/256/{z}/{x}/{y}@2x?access_token=${process.env.REACT_APP_MAPBOX_TOKEN}`} 
+            />
+                
+            <Marker 
+                icon={mapIcon}
+                position={[-23.5229859, -46.6995142]}
+            />
         </Map>
 
         <Link to="" className="create-orphanpage">
